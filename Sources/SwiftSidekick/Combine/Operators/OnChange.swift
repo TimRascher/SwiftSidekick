2b8e76aaae8becf506466cb59f7857b102907460
Timothy Rascher <timrascher@gmail.com>
2022-09-29 11:43:23 -0700

    Added in Combine Operator to only return when new element is different from last element.

---
+++ Sources/SwiftSidekick/Combine/Operators/OnChange.swift
@@ -0,0 +1,70 @@
+//
+//  OnChange.swift
+//
+//  Created by Timothy Rascher on 9/29/22.
+//
+
+import Foundation
+import Combine
+
+extension Publishers {
+    public struct OnChange<UpStream> where UpStream: Publisher, UpStream.Output: Equatable {
+        public typealias Output = UpStream.Output
+        public typealias Failure = UpStream.Failure
+        private let upStream: UpStream
+        
+        public init(_ upStream: UpStream) {
+            self.upStream = upStream
+        }
+    }
+}
+extension Publishers.OnChange: Publisher {
+    public func receive<S>(subscriber: S) where S: Subscriber, S.Input == Output, S.Failure == Failure {
+        
+    }
+}
+public extension Publishers.OnChange {
+    class Sub<S> where S: Subscriber, S.Input == Output, S.Failure == Failure {
+        private let publisher: Publishers.OnChange<UpStream>
+        private let subscriber: S
+        private var upStreamSub: Subscription?
+        private var lastDemand: Subscribers.Demand?
+        private var lastValue: Input?
+        
+        private init(_ publisher: Publishers.OnChange<UpStream>, _ subscriber: S) {
+            self.publisher = publisher
+            self.subscriber = subscriber
+            publisher.upStream.subscribe(self)
+        }
+    }
+}
+extension Publishers.OnChange.Sub: Subscription {
+    public func request(_ demand: Subscribers.Demand) {
+        lastDemand = demand
+        guard let upStreamSub = upStreamSub else { return }
+        upStreamSub.request(demand)
+    }
+    public func cancel() {
+        upStreamSub?.cancel()
+    }
+}
+extension Publishers.OnChange.Sub: Subscriber {
+    public typealias Input = UpStream.Output
+    public typealias Failure = UpStream.Failure
+    public func receive(subscription: Subscription) {
+        upStreamSub = subscription
+        guard let demand = lastDemand else { return }
+        subscription.request(demand)
+    }
+    private var demand: Subscribers.Demand { lastDemand ?? .none }
+    public func receive(_ input: UpStream.Output) -> Subscribers.Demand {
+        if let lastValue = lastValue, lastValue == input { return demand }
+        lastValue = input
+        let demand = subscriber.receive(input)
+        lastDemand = demand
+        return demand
+    }
+    public func receive(completion: Subscribers.Completion<UpStream.Failure>) {
+        subscriber.receive(completion: completion)
+    }
+}
